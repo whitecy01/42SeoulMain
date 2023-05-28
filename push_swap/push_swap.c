eb43e6589449f119daf96b51bbc9931d1faffdb3
JaeYoonJung08 <whiltecy01@naver.com>
2023-05-28 21:56:20 +0900

    push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jaeyojun <jaeyojun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 14:19:08 by jaeyojun          #+#    #+#             */
-/*   Updated: 2023/05/26 21:25:26 by jaeyojun         ###   ########seoul.kr  */
+/*   Updated: 2023/05/28 21:49:50 by jaeyojun         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 
 
 
-// void	print_error(int error)
-// {
-// 	if (error == -1)
-// 		write(2, "Error\n", 6);
-// 	exit(1);
-// }
+void	print_error(int error)
+{
+	if (error == -1)
+		write(2, "Error\n", 6);
+	exit(1);
+}
 
 // int	input_check_string(char *argv)
 // {
@@ -119,9 +119,11 @@
 int	check_input_separator(char c)
 {
 	if ('0' <= c && '9' >= c)
+	//if (c == 32)
 		return (0);
 	return (1);
 }
+
 
 int	check_input_size(char *argv)
 {
@@ -145,114 +147,189 @@ int	check_input_size(char *argv)
 	return (count);
 }
 
-// char	*input_split_argv(char **argv, int array_size)
-// {
-
-// }
-
-int	split_len(char **s, int start, int two_start)
+int	string_check(char s)
 {
-	int i;
-	int len;
-
-	len = 0;
-	while (s[start][two_start])
-	{
-		if (s[start][two_start] >= '0' && '9' >= s[start][two_start])
-			len++;
-	}
-	return (len);
+	if (s == 32)
+		return (1);
+	return (0);
 }
 
-char	*ft_substr(char **s, unsigned int start, int two_start)
+int	ft_strlen(const char *s, int start)
 {
-	size_t			i;
-	char			*temp;
-	size_t			cou;
-
-	cou = ft_strlen(s);
-	len = split_len(s, start);
-	if (cou <= start)
-		return (ft_strdup(""));
-	if (len >= cou - start)
-		len = cou - start;
-	temp = (char *)malloc(sizeof(char) * (len + 1));
-	if (!temp)
-		return (0);
+	int i;
+	
 	i = 0;
-	while (s[start][two_start] != '\0' && i < len)
+	if (!s)
+		return (0);
+	while (s[start] != '\0' && s[start] != 32)
 	{
-		temp[i] = s[start][two_start];
+		if (s[start] == 32)
+			break ;
 		start++;
 		i++;
 	}
+	return (i);
+}
+
+
+char	*ft_substr(char *s, int *start)
+{
+	int	i;
+	char			*temp;
+
+	//printf("start %d\n ", *start);
+	// if (cou <= start)
+	// 	return (ft_strdup(""));
+	// if (len >= cou - start)
+	// 	len = cou - start;
+	//printf("start : %d , s :%s\n", *start, s);
+	temp = (char *)malloc((sizeof(char) * ft_strlen(s, *start)) + 1);
+	//printf("len : %d, start : %d\n", ft_strlen(s, *start), *start);
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (s[*start] != '\0' && s[*start] != 32)
+	{
+		temp[i] = s[*start];
+		//printf("tmp : %c\n", temp[i]);
+		(*start)++;
+		i++;
+	}
+	//printf("start : %d , s :%s\n", *start, s);
+	// while (i < *start)
+	// {
+	// 	temp[i] = s[*start];
+	// 	(*start)++;
+	// 	i++;
+	// }
 	temp[i] = '\0';
+	//printf("tmp_full: %s\n", temp);
 	return (temp);
 }
 
-char	**input_split(char **argv, int array_size)
+// char	**input_split_argv(char **str, char *argv, int *two_malloc_count)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	 //*str[i] = "awd";
+// 	// printf("str %s\n", *str[i]);
+// 	// printf("argv %s\n", argv);
+// 	//  printf("two_malloc_count : %d\n", *two_malloc_count);
+// 	while (argv[i])
+// 	{
+// 		while (argv[i] && string_check(argv[i]) == 1)
+// 		{
+// 			printf("i1 : %d\n", i);
+// 			i++;
+// 		}
+// 		while (argv[i] && string_check(argv[i]) == 0)
+// 		{
+// 			printf("i : %d\n", i);
+// 			printf("two_malloc_count : %d\n", *two_malloc_count);
+// 			(*str)[*two_malloc_count] = ft_substr(argv, &i);
+// 			//printf("str[%d] : %s\n", *two_malloc_count, *str[*two_malloc_count]);
+// 			printf("str : %s\n", *str[*two_malloc_count]);
+// 			(*two_malloc_count)++;
+			
+// 		}
+// 	}
+// 	return (str);
+// }
+
+char	**input_split_argv(char **str, char *argv, int *two_malloc_count)
+{
+	int i;
+
+	i = 0;
+	 //*str[i] = "awd";
+	// printf("str %s\n", *str[i]);
+	 //printf("ㄱ기모지 argv %s\n", argv);
+	//  printf("two_malloc_count : %d\n", *two_malloc_count);
+	while (argv[i])
+	{
+		while (argv[i] && string_check(argv[i]) == 1)
+		{
+			//printf("i1 : %d\n", i);
+			i++;
+		}
+		while (argv[i] && string_check(argv[i]) == 0)
+		{
+			//printf("i : %d\n", i);
+			//printf("two_malloc_count : %d\n", *two_malloc_count);
+			str[*two_malloc_count] = ft_substr(argv, &i);
+			//printf("str[%d] : %s\n", *two_malloc_count, *str[*two_malloc_count]);
+			//printf("str : %s\n", str[*two_malloc_count]);
+			(*two_malloc_count)++;
+			
+		}
+	}
+	return (str);
+}
+
+char	**input_split(char **argv, int argc, int array_size)
 {
 	char	**str;
 	int 	i;
 	int		j;
 
 	j = 0;
-	i = 0;
+	i = 1;
 	str = (char **)malloc(array_size * sizeof(char *) + 1);
-	while (i < array_size)
+	// printf("%s", argv[i]);
+	printf("array_size : %d\n", array_size);
+	while (i < argc)
 	{
-		while (argv[i][j])
-		{
-			str[i] = ft_substr(argv, i, j);
-			j += split_len(argv, i, j);
-			if (!(argv[i][j]))
-				break ;
-			j++;
-		}
+		// if (!(*(argv + i)))
+		// 	break ;
+		//printf("i_increasing: %d\n", i);
+		//input_split_argv(&str, *(argv + i), &j);
+		str = input_split_argv(str, *(argv + i), &j);
 		i++;
 	}
-	str[i] = 0;
+	str[array_size] = 0;
+	
+	i= 0;
+	while (i < array_size)
+	{
+		//printf("str: %s\n", str[i]);
+		i++;
+	}
 	return (str);
 }
-
-
-
 
 char	**check_input_spilt(int argc, char **argv, int *array_size)
 {
 	char	**str;
+	int		i;
 
 	i = 1;
 	while (i < argc)
-	array_size += check_input_size(argv[i++]);
-	// str = (char **)malloc(array_size * sizeof(char *) + 1);
+		*array_size += check_input_size(argv[i++]);
 	//2차원 자체를 만들고 넣어줘야함.
-	str = input_split(argv, array_size);
+	str = input_split(argv, argc, *array_size);
+	return (str);
 }
-
-
 
 int	main(int argc, char **argv)
 {
-	//int		array_size;
+	int		array_size;
 	//int		*array;
 	char	**array_split;
-	
 
-	// if (argc < 2)
-	// 	print_error(-1);
-
-	//1. split으로 개수 세고 1차원 배열에 넣기 
-	//입력값 처리, a에 넣어야하나?
+	if (argc < 2)
+		print_error(-1);
+	array_size = 0;
+	//1. 2차원을 말록한 후에 거기에다가 값을 넣기
 	array_split = check_input_spilt(argc, argv, &array_size);
-	
-	// 2. atoi로 숫자 문자열이 잘 들어오는지 확인
-	input_check(argc, argv);
+	int i = 0;
+	while (i < array_size)
+	{
+		//printf("array_split: %s\n", array_split[i]);
+		i++;
+	}
 
-	// printf("array : %d\n" , array_size);
-	// for (int i = 0; i < argc; i++)
-	// {
-	// 	printf("%s\n", argv[i]);
-	// }
+	// 2. atoi로 숫자 문자열이 잘 들어오는지 확인
+	printf("-------------------\n");
 	printf("argc : %d\n", argc);
 }
