918e81ca7b8736a610cc7ea800f443d5ff319bc8
JaeYoonJung08 <whiltecy01@naver.com>
2023-05-29 21:17:57 +0900

    push_swap

---
+++ push_swap/create_stacknode.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_stacknode.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeyojun <jaeyojun@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 20:25:19 by jaeyojun          #+#    #+#             */
+/*   Updated: 2023/05/29 21:10:50 by jaeyojun         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "push_swap.h"
+
+// t_info	*create_stack(int array_size)
+// {
+// 	t_info	*tmp;
+
+// 	tmp = (t_stack *)malloc(sizeof(t_stack));
+// 	tmp->size = array_size;
+// 	tmp->top = NULL;
+// 	tmp->bottom = NULL;
+// 	return (tmp);
+// }
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,18 +6,12 @@
 /*   By: jaeyojun <jaeyojun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 14:19:08 by jaeyojun          #+#    #+#             */
-/*   Updated: 2023/05/28 21:49:50 by jaeyojun         ###   ########seoul.kr  */
+/*   Updated: 2023/05/29 21:14:12 by jaeyojun         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
 #include <stdio.h>
-
-
-
-
-
 
 void	print_error(int error)
 {
@@ -26,104 +20,12 @@ void	print_error(int error)
 	exit(1);
 }
 
-// int	input_check_string(char *argv)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	//printf("argv : %s\n", argv);
-// 	//공백 체크 하면서 그 부분에 이상한 문자 있는지 검사 
-// 	while (argv[i] == 32)
-// 	{
-// 		printf("debug argv[%c]\n", argv[i]);
-// 		if ((argv[i] < '0' && argv[i] > '9'))
-// 		{
-// 			return (0);
-// 		}
-// 		i++;
-// 	}
-// 	if ((argv[i] < '0' && argv[i] > '9'))
-// 	{
-// 			return (0);
-// 	}
-// 	while (argv[i] >= '0' && argv[i] <= '9')
-// 	{
-// 		if ((argv[i] < '0' && argv[i] > '9'))
-// 		{
-// 			return (0);
-// 		}
-// 		i++;
-// 	}
-// 	while (argv[i] == 32)
-// 	{
-// 		if ((argv[i] < '0' && argv[i] > '9'))
-// 			return (0);
-// 		i++;
-// 	}
-// 	printf("---------\n");
-// 	return (1);
-// }
-
-// void	input_check(int argc, char **argv)
-// {
-// 	int	i;
-
-// 	i = 1;
-// 	while (i < argc)
-// 	{
-// 		if (input_check_string(*(argv + i)) == 0)
-// 			return (print_error(-1));
-// 		i++;
-// 	}
-// }
-
-// int	check_input_size(int argc, char **argv)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	count;
-// 	int	check;
-
-// 	count = 0;
-// 	i = 0;
-// 	while (i < argc)
-// 	{
-// 		j = 0;
-// 		while (argv[i][j] != '\0')
-// 		{
-// 			if ('0' <= argv[i][j] && argv[i][j] <= '9')
-// 			{
-// 				check = j;
-// 				check++;
-// 				count++;
-// 				printf("i : %d check : %d\n", i, check);
-// 				while ('0' <= check && check <= '9')
-// 				{
-// 					if ('0' > check && check > '9')
-// 					{
-// 						j = check;
-// 						break ;
-// 					}
-// 					check++;
-// 					printf("check : %d", check);
-// 				}
-// 			}
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	return (count);
-// }
-
-
 int	check_input_separator(char c)
 {
-	//if ('0' <= c && '9' >= c)
 	if (c == 32)
 		return (1);
 	return (0);
 }
-
 
 int	check_input_size(char *argv)
 {
@@ -156,8 +58,8 @@ int	string_check(char s)
 
 int	ft_strlen(const char *s, int start)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
 	if (!s)
 		return (0);
@@ -171,97 +73,40 @@ int	ft_strlen(const char *s, int start)
 	return (i);
 }
 
-
 char	*ft_substr(char *s, int *start)
 {
-	int	i;
-	char			*temp;
+	int		i;
+	char	*temp;
 
-	//printf("start %d\n ", *start);
-	// if (cou <= start)
-	// 	return (ft_strdup(""));
-	// if (len >= cou - start)
-	// 	len = cou - start;
-	//printf("start : %d , s :%s\n", *start, s);
 	temp = (char *)malloc((sizeof(char) * ft_strlen(s, *start)) + 1);
-	//printf("len : %d, start : %d\n", ft_strlen(s, *start), *start);
 	if (!temp)
 		return (NULL);
 	i = 0;
 	while (s[*start] != '\0' && s[*start] != 32)
 	{
 		temp[i] = s[*start];
-		//printf("tmp : %c\n", temp[i]);
 		(*start)++;
 		i++;
 	}
-	//printf("start : %d , s :%s\n", *start, s);
-	// while (i < *start)
-	// {
-	// 	temp[i] = s[*start];
-	// 	(*start)++;
-	// 	i++;
-	// }
 	temp[i] = '\0';
-	//printf("tmp_full: %s\n", temp);
 	return (temp);
 }
 
-// char	**input_split_argv(char **str, char *argv, int *two_malloc_count)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	 //*str[i] = "awd";
-// 	// printf("str %s\n", *str[i]);
-// 	// printf("argv %s\n", argv);
-// 	//  printf("two_malloc_count : %d\n", *two_malloc_count);
-// 	while (argv[i])
-// 	{
-// 		while (argv[i] && string_check(argv[i]) == 1)
-// 		{
-// 			printf("i1 : %d\n", i);
-// 			i++;
-// 		}
-// 		while (argv[i] && string_check(argv[i]) == 0)
-// 		{
-// 			printf("i : %d\n", i);
-// 			printf("two_malloc_count : %d\n", *two_malloc_count);
-// 			(*str)[*two_malloc_count] = ft_substr(argv, &i);
-// 			//printf("str[%d] : %s\n", *two_malloc_count, *str[*two_malloc_count]);
-// 			printf("str : %s\n", *str[*two_malloc_count]);
-// 			(*two_malloc_count)++;
-			
-// 		}
-// 	}
-// 	return (str);
-// }
-
 char	**input_split_argv(char **str, char *argv, int *two_malloc_count)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	 //*str[i] = "awd";
-	// printf("str %s\n", *str[i]);
-	 //printf("ㄱ기모지 argv %s\n", argv);
-	//  printf("two_malloc_count : %d\n", *two_malloc_count);
 	while (argv[i])
 	{
 		while (argv[i] && string_check(argv[i]) == 1)
 		{
-			//printf("i1 : %d\n", i);
 			i++;
 		}
 		while (argv[i] && string_check(argv[i]) == 0)
 		{
-			//printf("i : %d\n", i);
-			//printf("two_malloc_count : %d\n", *two_malloc_count);
 			str[*two_malloc_count] = ft_substr(argv, &i);
-			//printf("str[%d] : %s\n", *two_malloc_count, *str[*two_malloc_count]);
-			//printf("str : %s\n", str[*two_malloc_count]);
 			(*two_malloc_count)++;
-			
 		}
 	}
 	return (str);
@@ -270,31 +115,19 @@ char	**input_split_argv(char **str, char *argv, int *two_malloc_count)
 char	**input_split(char **argv, int argc, int array_size)
 {
 	char	**str;
-	int 	i;
+	int		i;
 	int		j;
 
 	j = 0;
 	i = 1;
 	str = (char **)malloc(array_size * sizeof(char *) + 1);
-	// printf("%s", argv[i]);
 	printf("array_size : %d\n", array_size);
 	while (i < argc)
 	{
-		// if (!(*(argv + i)))
-		// 	break ;
-		//printf("i_increasing: %d\n", i);
-		//input_split_argv(&str, *(argv + i), &j);
 		str = input_split_argv(str, *(argv + i), &j);
 		i++;
 	}
 	str[array_size] = 0;
-	
-	i= 0;
-	while (i < array_size)
-	{
-		//printf("str: %s\n", str[i]);
-		i++;
-	}
 	return (str);
 }
 
@@ -311,46 +144,100 @@ char	**check_input_spilt(int argc, char **argv, int *array_size)
 	return (str);
 }
 
-t_info 	*new_s_number(char **array_split, int array_size)
+// t_info 	*new_s_number(char **array_split, int array_size)
+// {
+// 	t_info *start;
+// 	t_info *loc;
+
+// //노드를 추가 한후 붙여주야 할 듯
+// 	start = (t_info *)malloc(sizeof(t_info));
+// 	loc = start;
+// 	start->content = 10;
+// 	start->next =
+
+// 	while (array_size)
+// 	{
+// 		start = (t_info *)malloc(sizeof(t_info));
+// 		loc = start;
+// 		start->content = 10;
+// 		start->next = 
+// 		array_size--;
+// 	}
+
+// }
+
+int ft_atoi(char *str)
 {
-	t_info *start;
-	t_info *loc;
+	int	i;
+	int	result;
+	int	min;
 
-//노드를 추가 한후 붙여주야 할 듯
-	start = (t_info *)malloc(sizeof(t_info));
-	loc = start;
-	start->content = 10;
-	start->next =
-
-	while (array_size)
+	i = 0;
+	min = 1;
+	if (str[i] == 32)
+		print_error(-1);
+	if (str[i] == '-')
 	{
-		start = (t_info *)malloc(sizeof(t_info));
-		loc = start;
-		start->content = 10;
-		start->next = 
-		array_size--;
+		min = -1;
+		i++;
 	}
+	result = 0;
+	while (str[i])
+	{
+		if ('0' <= str[i] && str[i] <= '9')
+			result = (10 * result) + str[i] - '0';
+		else
+			print_error(-1);
+		i++;
+	}
+	return (result * min);
+}
 
+int		*change_int(char **array_split, int array_size, int *int_array_len)
+{
+	int *temp;
+	int	i;
+
+	temp = (int *)malloc(sizeof(int) * array_size);
+	i = 0;
+	while (i < array_size)
+	{
+		temp[i] = ft_atoi(array_split[i]);
+		i++;
+	}
+	i = 0;
+	while (temp[i])
+	{
+		(*int_array_len)++;
+		i++;
+	}
+	return (temp);
 }
 
 int	main(int argc, char **argv)
 {
-	int		array_size;
-	//int		*array;
-	char	**array_split;
-	t_info 	*s_number;
+	int			array_size;
+	char		**array_split;
+	int			*array;
+	int			int_array_len;
+	t_info		*s;
 
 	if (argc < 2)
 		print_error(-1);
 	array_size = 0;
 	//1. 2차원을 말록한 후에 거기에다가 값을 넣기
 	array_split = check_input_spilt(argc, argv, &array_size);
-	int i = 0;
-	while (i < array_size)
-	{
-		printf("array_split: %s\n", array_split[i]);
-		i++;
-	}
+	if (array_size == 0)
+		print_error(-1);
+	//2. char 2차원 배열을 1차원 배열에 넣어주기
+	array = change_int(array_split, array_size, &int_array_len);
+	//printf("int_array_size : %d\n", int_array_len);
+	//3. a,b 스택 만들기
+	// a = create_stack(int_array_len);
+	// b = create_stack(int_array_len);
+	// printf("a->size : %d", a->size);
+	//4. int array를 a스택에 넣기
+	
 	
 	// s_number = NULL;
 	// s_number = new_s_number(array_split, array_size);
