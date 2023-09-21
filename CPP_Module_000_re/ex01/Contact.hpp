7d2ce66c30d8d85a26e55a90787b0589a034d25c
JaeYoonJung08 <whiltecy01@naver.com>
2023-09-21 15:00:29 +0900

    CPP_Mo

---
+++ CPP_Module_000_re/ex01/Contact.hpp
@@ -0,0 +1,31 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+
+
+class Contact{
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string phone_number;
+	std::string darkset_secret;
+public:
+
+	void	setFirstName(std::string First_Name);
+	void	setLastName(std::string last_name);
+	void	setNickName(std::string nick_name);
+	void	setPhoneNumber(std::string phone_number);
+	void	setDarkestSecret(std::string darkset_secret);
+
+	std::string getFirstName();
+	std::string getLastName();
+	std::string getNickName();
+	std::string getPhoneNumber();
+	std::string getDarkestSecret();
+
+	void	setAllContact(Contact contact);
+};
+
+#endif
